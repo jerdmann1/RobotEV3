35d8ae017f7a8e484e77faa0db1450304f7fe480
jerdmann1 <justinerdmann@Justins-MacBook-Pro.local>
2020-11-18 11:55:35 -0600

    added wander

--- RobotEV3/src/RobotEV3.c
+++ RobotEV3/src/RobotEV3.c
@@ -5,7 +5,7 @@
 
 char current_speed = 20;
 int optimal = 20;
-int current_time = 0, search_interval = 15;
+int current_time = 0, search_interval = 15, wander_search_interval = 300;
 
 void turn_right(int amount){
 	OnFwdReg(OUT_B, (char)(current_speed + amount));
@@ -60,30 +60,27 @@ void wall_follow(){
 }
 
 bool wander(){
-	int ref_sensor = ReadSensor(IN_3);
-	if(ref_sensor <= optimal){
-		OnRevReg(OUT_B, (char)20);
-		OnRevReg(OUT_C, (char)20);
-		usleep(500000);
-		OnFwdReg(OUT_B, (char)20);
-		OnRevReg(OUT_C, (char)20);
-		usleep(1500000);
-		OnFwdReg(OUT_B, current_speed);
-		OnFwdReg(OUT_C, current_speed);
-		return true;
-	}
-	return false;
+	ResetRotationCount(OUT_B);
+	OnFwdReg(OUT_B, (char)current_speed);
+	OnRevReg(OUT_C, (char)current_speed);
+	while(MotorRotationCount(OUT_B) < 180);
+	OnRevReg(OUT_B, current_speed);
+	OnRevReg(OUT_C, current_speed);
+	usleep(600000);
+	OnFwdReg(OUT_B, current_speed/2);
+	OnFwdReg(OUT_C, current_speed/2);
+	while(ReadSensor(IN_3) > 10);
 }
 
-bool look_for_can(){
+bool look_for_can(int search_distance){
 	ResetRotationCount(OUT_B);
 	OnFwdReg(OUT_B, (char)10);
 	OnRevReg(OUT_C, (char)10);
 	while(MotorRotationCount(OUT_B) < 720){
 		int heading = ReadSensor(IN_1);
-		if(heading < 1000){
+		if(heading < search_distance){
 			ResetRotationCount(OUT_B);
-			while(ReadSensor(IN_1) < 2549);
+			while(ReadSensor(IN_1) < 1200);
 			int final_count = MotorRotationCount(OUT_B);
 			OnFwdReg(OUT_C, (char)10);
 			OnRevReg(OUT_B, (char)10);
@@ -101,64 +98,53 @@ bool look_for_can(){
 	return false;
 }
 
+void goal_find(){
+	SetAllSensorMode(US_DIST_MM, NO_SEN, COL_COLOR, NO_SEN);
+	while(true){
+		int color_sensor = ReadSensor(IN_3);
+		if(color_sensor == 2){
+			SetAllSensorMode(US_DIST_MM, NO_SEN, COL_REFLECT, NO_SEN);
+			break;
+		}else if(color_sensor == 5){
+			Off(OUT_B);
+			Off(OUT_C);
+			PlayTone(TONE_C2, NOTE_WHOLE);
+			usleep(1000000);
+			ResetRotationCount(OUT_B);
+			OnFwdReg(OUT_B, (char)10);
+			OnRevReg(OUT_C, (char)10);
+			while(MotorRotationCount(OUT_B) < 360);
+			look_for_can(230);
+			ResetRotationCount(OUT_B);
+			while(MotorRotationCount(OUT_B) < 1000);
+			exit(0);
+		}
+	}
+}
+
 int main(void)
 {
 	ButtonWaitForPress(BTNCENTER);
 	SetAllSensorMode(US_DIST_MM, NO_SEN, COL_REFLECT, NO_SEN);
-	time_t start;
+	time_t start, wander_start;
 	bool follow = true;
 	OnFwdReg(OUT_B, current_speed);
 	OnFwdReg(OUT_C, current_speed);
 	while(ReadSensor(IN_3) > 10);
 	start = time(NULL);
+	wander_start = time(NULL);
 	while(1){
-		if(follow){
-			wall_follow();
-		}
-		int time_passed = time(NULL) - start;
-		if(time_passed >= search_interval){
-			if(look_for_can()){
-				SetAllSensorMode(US_DIST_MM, NO_SEN, COL_COLOR, NO_SEN);
-				while(true){
-					int color_sensor = ReadSensor(IN_3);
-					if(color_sensor == 2){
-						SetAllSensorMode(US_DIST_MM, NO_SEN, COL_REFLECT, NO_SEN);
-						break;
-					}else if(color_sensor == 5){
-						Off(OUT_B);
-						Off(OUT_C);
-						PlayTone(TONE_C2, NOTE_WHOLE);
-						usleep(1000000);
-						ResetRotationCount(OUT_B);
-						OnFwdReg(OUT_B, (char)10);
-						OnRevReg(OUT_C, (char)10);
-						while(MotorRotationCount(OUT_B) < 360);
-						ResetRotationCount(OUT_B);
-						OnFwdReg(OUT_B, (char)10);
-						OnRevReg(OUT_C, (char)10);
-						while(MotorRotationCount(OUT_B) < 720){
-							int heading = ReadSensor(IN_1);
-							if(heading < 230){
-								ResetRotationCount(OUT_B);
-								while(ReadSensor(IN_1) < 2549);
-								int final_count = MotorRotationCount(OUT_B);
-								OnFwdReg(OUT_C, (char)10);
-								OnRevReg(OUT_B, (char)10);
-								while(MotorRotationCount(OUT_B) > ((final_count) / 2));
-								OnRevReg(OUT_B, current_speed);
-								OnRevReg(OUT_C, current_speed);
-								usleep(400000);
-								ResetRotationCount(OUT_B);
-								OnFwdReg(OUT_B, current_speed);
-								OnFwdReg(OUT_C, current_speed);
-								while(MotorRotationCount(OUT_B) < 1000);
-								exit(0);
-							}
-						}
-					}
-				}
+		wall_follow();
+		int search_time_passed = time(NULL) - start;
+		int wander_time_passed = time(NULL) - wander_start;
+		if(search_time_passed >= search_interval){
+			if(look_for_can(1000)){
+				goal_find();
 			}
-			start = time(NULL) + 3;
+			start = time(NULL);
+		}else if(wander_time_passed >= wander_search_interval){
+			wander();
+			wander_start = time(NULL);
 		}
 	}
 	return 0;
